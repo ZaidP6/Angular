a1895228dfaad22c1abf11592045e38067d3423b
ZaidP6 <pilarads06@gmail.com>
2024-10-15 12:31:47 +0200

    calculadora clase

---
+++ two-way-data-binding/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './components/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ two-way-data-binding/src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  // 1. Propeerty binding (one way)
+  valoracion = 5;
+
+  imgUrl= "https://i.pinimg.com/originals/d7/0a/e5/d70ae5e2bf047eedc063dc8486df7e48.png";
+
+  // 2. Event binding (one way)
+  showUserInfo(){
+    alert("Nombre del usuario: " + this.nombre);
+  }
+
+  // 3 . Two way data binding
+  nombre = 'Zaid';
+
+  
+}
