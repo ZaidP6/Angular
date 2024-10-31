164dcb7bf40384cb90b61c445354c23a540b8184
ZaidP6 <pilarads06@gmail.com>
2024-10-31 14:33:35 +0100

    sigo con problemas de bootstrap

---
+++ FormularioV2/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormularioRegistroComponent } from './components/formulario-registro/formulario-registro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormularioRegistroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
