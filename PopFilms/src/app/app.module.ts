54cd4c722b62d236727de4a7742db0a70a1386fb
ZaidP6 <pilarads06@gmail.com>
2024-11-11 22:03:44 +0100

    pop-films html en progreso

--- PopFilms/src/app/app.module.ts
+++ PopFilms/src/app/app.module.ts
@@ -1,24 +1,31 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PopWebDesignedComponent } from './components/pop-web-designed/pop-web-designed.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './modules/material/material.module';
+import { PopWebDesignedComponent } from './components/pop-web-designed/pop-web-designed.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PopWebDesignedComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    MaterialModule,
+    FormsModule,
+    PopWebDesignedComponent
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
