be05f71b0a8ca3ecf28170b3e6aeafb2666ef498
ZaidP6 <pilarads06@gmail.com>
2024-11-15 12:40:42 +0100

    uso token y apikey

--- PopFilms/src/app/services/pop-films.service.ts
+++ PopFilms/src/app/services/pop-films.service.ts
@@ -2,27 +2,28 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+
+const API_KEY = '4c92ea126ceabbca4fbdaa0e7e3696ca';
+const ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzkyZWExMjZjZWFiYmNhNGZiZGFhMGU3ZTM2OTZjYSIsIm5iZiI6MTczMTY2Nzk4NS41Njg0ODg2LCJzdWIiOiI2NzMxYmUwNjdlZjJjMzFkNzhlZGFiZjkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.5BGQdjBLo4-I1JsQE9zaFOoXmk-VFhcIyh6AspmhX50';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PopFilmsService {
 
-  private apiUrl = 'https://api.themoviedb.org/'; // URL base de TMDb
-  private token = '4c92ea126ceabbca4fbdaa0e7e3696ca'; // Reemplaza con tu token
+  private apiUrl = 'https://api.themoviedb.org/'; 
 
   constructor(private http: HttpClient) { }
 
   getPopularMovies(): Observable<any> {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${this.token}`
-    });
-    return this.http.get(`${this.apiUrl}/movie/popular`, { headers });
+    
+    return this.http.get(`${this.apiUrl}/movie/popular?api_key=${API_KEY}`);
   }
 
   // Método para buscar películas por nombre
   searchMovies(query: string): Observable<any> {
     const headers = new HttpHeaders({
-      Authorization: `Bearer ${this.token}`
+      Authorization: `Bearer ${ACCESS_TOKEN}`
     });
     return this.http.get(`${this.apiUrl}/search/movie?query=${query}`, { headers });
   }
